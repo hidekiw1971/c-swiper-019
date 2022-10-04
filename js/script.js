7a3c89e6ea65151f3bc6c9dbb145ca3460737349
hideki watabe <hideki.w.1971@gmail.com>
2022-10-04 20:54:35 +0900

    2022/10/4 パラメータセットしたら連動しなくなる状態。

--- js/script.js
+++ js/script.js
@@ -1,32 +1,21 @@
 jQuery(function ($) {
   // この中であればWordpressでも「$」が使用可能になる
-  // start
-  //メインスライド
-  var slider = new Swiper(".gallery-slider", {
+  const mySwiper_thumb = new Swiper(".gallery-thumbs", {
+    slidesPerView: 6,
+    centeredSlides: true,
+    loop: true,
+  });
+
+  const mySwiper_main = new Swiper(".gallery-slider", {
     slidesPerView: 1,
     centeredSlides: true,
     loop: true,
-    loopedSlides: 6, //スライドの枚数と同じ値を指定
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    // thumbs: {
-    //   swiper: thumbs,
-    // },
+    thumbs: {
+      swiper: mySwiper_thumb,
+    },
   });
-
-  //サムネイルスライド
-  var thumbs = new Swiper(".gallery-thumbs", {
-    // slidesPerView: "auto",
-    slidesPerView: 6,
-    spaceBetween: 5,
-    centeredSlides: true,
-    loop: true,
-    slideToClickedSlide: true,
-  });
-
-  slider.controller.control = thumbs;
-  thumbs.controller.control = slider;
-  // end
-});
+}); // end
